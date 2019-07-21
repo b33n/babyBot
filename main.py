71beb58e0d3ca9354c7dc18f014d96383adffc81
Benjamin <benjamin.lanehart1@gmail.com>
2019-07-21 10:44:22 -0400

    Initial Version of the bot

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("Hello World")
+from discord.ext import commands
+import creds
+
+client = commands.Bot(command_prefix = '.')
+cryGif = 'https://giphy.com/gifs/cry-baby-TL2Yr3ioe78tO'
+
+@client.event
+async def on_ready():
+    print('Bot is Ready')
+
+
+@client.event
+async def on_message(message):
+    author = message.author
+    content = message.content
+    channel = message.channel
+    authorID = message.author.id
+    print('{}: {}, {}, {}'.format(author, content, channel, authorID))
+    if str(authorID) in creds.usersCry:
+        print('Yes')
+        await message.channel.send(cryGif)
+
+    elif author not in creds.usersCry:
+        print('No')
+
+client.run(creds.tokenID)
