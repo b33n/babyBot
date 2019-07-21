a007f6db70e0771a49cd74ff8c9443019fd6b6cd
Benjamin <benjamin.lanehart1@gmail.com>
2019-07-21 11:00:08 -0400

    Initial Version of the bot

--- creds.py
+++ creds.py
@@ -1,2 +1,7 @@
-tokenID = 'NTMyNjA4MDUyNzI2MjAyMzc5.DxfS_Q.vBGkZGhlBcEs5xS6kZrlqc1Wg_E'
+import sys
+sys.path.insert(0, 'C:\localFiles')
+
+import localFile.py
+
+tokenID = localFile.tokenID
 usersCry = ['404236458346545163', '253546480416063489', '283288506631979008']
